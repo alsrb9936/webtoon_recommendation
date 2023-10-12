47abb28dc496ce159ab3525d4bff22550774a9ae
PushGyu <ghfn123@gmail.com>
2023-10-12 23:52:53 +0000

    1013 update

--- webtoon/views.py
+++ webtoon/views.py
@@ -6,26 +6,28 @@ from django.http import HttpResponse
 from django.db.models import Q
 from .models import Webtoon, User_Webtoon
 from .forms import User_WebtoonForm
+from collections import Counter
+from .secret import gpt_api_key
 
+import openai
 import csv
 
+
+# OpenAI API 인증 정보 설정
+openai.api_key = gpt_api_key
+
 def test(request):
-    # path = '/Users/jangmingyu/django/webtoon_recommendation_2/webtoon_com_3.csv'
-    # file = open(path)
-    # reader = csv.reader(file)
-    # list1 = []
-    # for n,row in enumerate(reader):
-    #     if n == 0:
-    #         continue
-    #     list1.append(Webtoon(
-    #         title=row[0], 
-    #         author=row[1], 
-    #         rating=float(row[2]), 
-    #         image=row[3], 
-    #         webtoon_url=row[4], 
-    #         genre=row[5],
-    #         week=row[6],))
-    # Webtoon.objects.bulk_create(list1)
+    page = request.GET.get('page', '1')
+    webtoon_list = User_Webtoon.objects.filter(reviewer=request.user).values('webtoon')
+    webtoon_list = Webtoon.objects.filter(id__in=webtoon_list).values('genre')
+
+    genre = []
+
+    for i in webtoon_list:
+        genre.extend(i['genre'][1:].split('#'))
+
+    genre = Counter(genre).most_common(3)
+    print(genre)
     return HttpResponse('test')
 
 def index(request):
@@ -136,9 +138,11 @@ def aireco(request):
     user_webtoon_list = User_Webtoon.objects.filter(reviewer=posible_user).values('webtoon')
     user_webtoon_list = Webtoon.objects.filter(id__in=user_webtoon_list)
 
-    if user_webtoon_list.count() <= 12:
+    if user_webtoon_list.count() <= 20:
         page = request.GET.get('page', '1')
-        webtoon_list = user_webtoon_list
+        webtoon_list = User_Webtoon.objects.filter(reviewer=request.user).values('webtoon')
+        webtoon_list = Webtoon.objects.filter(id__in=webtoon_list)
+
         paginator = Paginator(webtoon_list, 12)
         page_obj = paginator.get_page(page)
         context = {'webtoon_list': page_obj, 'posible_user': 'false'}
@@ -150,6 +154,7 @@ def aireco(request):
         webtoon_list = User_Webtoon.objects.filter(reviewer=request.user).values('webtoon')
         webtoon_list = Webtoon.objects.filter(id__in=webtoon_list)
 
+        genre_keyword = extract_genre(webtoon_list)
         # Ai 추천 코드
         # webtoon_list = recommendation(webtoon_list)
 
@@ -158,5 +163,41 @@ def aireco(request):
         context = {'webtoon_list': page_obj, 'posible_user': 'true'}
 
         return render(request, 'webtoon/webtoon_list.html', context)
+
+def extract_genre(webtoon_list):
+    genre = []
+
+    for i in webtoon_list:
+        genre.extend(i['genre'][1:].split('#'))
+    genre = Counter(genre).most_common(3)
+
+    return genre     
     
 ### def recommendation(webtoon_list):
+
+
+# GPT-3 모델 호출 및 결과 반환 함수
+def generate_chat_response(messages: list, user_input: str) -> str:
+    try:
+        messages.append({"role": "user", "content": f"{user_input}"})
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        assistant_content = completion.choices[0].message["content"].strip()
+        messages.append({"role": "assistant", "content": f"{assistant_content}"})
+        return assistant_content
+    except Exception as e:
+        assistant_content = "Error: " + str(e)
+        return assistant_content
+
+def chat(webtoon_genre: str):
+
+    # Request Body에서 입력 메시지 추출
+    inputText = f""" 입력하시오 {webtoon_genre}"""
+
+    # 이전 대화 기록을 저장할 리스트 생성
+    messages = []
+
+    # GPT-3 모델 호출 및 채팅 응답 생성
+    chat_response = generate_chat_response(messages, inputText)
+
+
+    return chat_response
